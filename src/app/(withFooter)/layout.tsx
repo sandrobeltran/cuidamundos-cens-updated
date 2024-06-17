1e7b9b84021b1842b13e8f64eda42433b42dbc21
Sandro Beltrán <devsandrobeltran@gmail.com>
2024-06-17 11:24:13 -0400

    removing google fonts

--- src/app/(withFooter)/layout.tsx
+++ src/app/(withFooter)/layout.tsx
@@ -1,13 +1,4 @@
 import Footer from "@/components/Footer";
-import type { Metadata } from "next";
-import { Ubuntu } from "next/font/google";
-
-const ubuntu = Ubuntu({
-  weight: ["300", "400", "500"],
-  subsets: ["latin"],
-  display: "swap",
-  adjustFontFallback: false,
-});
 
 export default function FooterLayout({
   children,
